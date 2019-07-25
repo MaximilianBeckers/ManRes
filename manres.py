bdaeb61c2c592ceb9e7f263948990af43d3976b0
mbeckers <maximilian.beckers@embl.de>
2019-07-25 14:14:19 +0200

    now also two half embeddings as inout

--- manres.py
+++ manres.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from Utilities import FSCutil,smallestEnclosingCircle
+from Utilities import FSCutil,smallestEnclosingCircle, FDRutil
 
 class ManRes:
 
-
 	halfMap1 = [];
 	halfMap2 = [];
+	fullMap = [];
+	filteredMap = [];
 	apix = 0.0;
 	frequencyMap = [];
 	FSCdata = [];
@@ -16,15 +17,17 @@ class ManRes:
 	embeddings = [];
 	sizeMap = 0;
 
-
 	#---------------------------------------------
-	def ManRes(self, embeddingData, size):
+	def ManRes(self, embeddingHalf1, embeddingHalf2, size):
 
 		np.random.seed(3);
-		self.embeddings = embeddingData;
+		
+		self.embeddingsHalf1 = embeddingHalf1;
+		self.embeddingsHalf2 = embeddingHalf2;
 		self.sizeMap = size;
 		self.make_half_maps();
-		self.standardizeResolution();
+		self.fullMap = self.halfMap1 + self.halfMap2;
+		self.standardizePixelSize();
 		self.frequencyMap = FSCutil.calculate_frequency_map(self.halfMap1);
 		maskData = np.ones(self.halfMap1.shape);
 
@@ -34,29 +37,54 @@ class ManRes:
 		self.FSCdata = FSC;
 		self.qVals = qVals_FDR;
 		self.resVec = tmpResVec;
-
+		self.filterMap();
 		self.writeFSC();
 
 		print(self.resolution);
+	#---------------------------------------------
+	def ManRes(self, embeddingData, size):
+
+		np.random.seed(3);
+
+		#split the localizations randomly in 2 half sets
+		numLocalizations = embeddingData.shape[0];
+		sizeHalfSet = int(numLocalizations/2);
+		permutedSequence = np.random.permutation(np.arange(numLocalizations));
+		self.embeddingsHalf1 = embeddingData[permutedSequence[0:sizeHalfSet], :];
+		self.embeddingsHalf2 = embeddingData[permutedSequence[sizeHalfSet:], :];
 
 
+		self.sizeMap = size;
+		self.make_half_maps();
+		self.fullMap = self.halfMap1 + self.halfMap2;
+		self.standardizePixelSize();
+		self.frequencyMap = FSCutil.calculate_frequency_map(self.halfMap1);
+		maskData = np.ones(self.halfMap1.shape);
+
+		tmpResVec, FSC, _, _, qVals_FDR, resolution_FDR, _ = FSCutil.FSC(self.halfMap1, self.halfMap2, maskData, self.apix, 0.143, 1, False, False, None);
+
+		self.resolution = resolution_FDR;
+		self.FSCdata = FSC;
+		self.qVals = qVals_FDR;
+		self.resVec = tmpResVec;
+		self.filterMap();
+		self.writeFSC();
+
+		print(self.resolution);
+                
 	#---------------------------------------------
 	def make_half_maps(self):
 
 		#initialize the half maps
 		tmpHalfMap1 = np.zeros((self.sizeMap, self.sizeMap));
 		tmpHalfMap2 = np.zeros((self.sizeMap, self.sizeMap));
-
-
-		numLocalizations = self.embeddings.shape[0];
-		sizeHalfSet = int(numLocalizations/2);
-
+		
 
 		#make the grid
-		minX = np.amin(self.embeddings[:,0]);
-		maxX = np.amax(self.embeddings[:,0]);
-		minY = np.amin(self.embeddings[:,1]);
-		maxY = np.amax(self.embeddings[:,1]);
+		minX = min(np.amin(self.embeddingsHalf1[:,0]), np.amin(self.embeddingsHalf2[:,0]));
+		maxX = max(np.amax(self.embeddingsHalf1[:,0]), np.amax(self.embeddingsHalf2[:,0]));
+		minY = min(np.amin(self.embeddingsHalf1[:,1]), np.amin(self.embeddingsHalf2[:,1]));
+		maxY = max(np.amax(self.embeddingsHalf1[:,1]), np.amax(self.embeddingsHalf2[:,1]));
 
 
 		spacingX = (maxX-minX)/float(self.sizeMap -1);
@@ -65,11 +93,9 @@ class ManRes:
 		spacing = max(spacingX, spacingY);
 		self.apix = spacing;
 
-		#split the localizations randomly in 2 half sets
-		permutedSequence = np.random.permutation(np.arange(numLocalizations));
-		half1 = self.embeddings[permutedSequence[0:sizeHalfSet], :];
-		half2 = self.embeddings[permutedSequence[sizeHalfSet:], :];
-
+		half1 = self.embeddingsHalf1;
+		half2 = self.embeddingsHalf2;
+                
 		print("make halfmap 1 ...");
 		#place localizations of HalfSet1
 		for i in range(half1.shape[0]):
@@ -120,12 +146,25 @@ class ManRes:
 
 
 	#---------------------------------------------
-	def standardizeResolution(self):
+	def standardizePixelSize(self):
 
 		#calculate convex hull
-		print("Calculate smalles enclosing circle ...");
-		hull = smallestEnclosingCircle.make_circle(self.embeddings);
+		print("Calculate smallest enclosing circle ...");
+		hull = smallestEnclosingCircle.make_circle(np.concatenate((self.embeddingsHalf1, self.embeddingsHalf2)));
 
+		#scale pixel size with respect to the radius of the smallest enclosing circle
 		self.apix = self.apix/hull[2];
+
+
+    #---------------------------------------------
+	def filterMap(self):
+
+		#fourier transform the full map
+		self.filteredMap = FDRutil.lowPassFilter(np.fft.rfftn(self.fullMap), self.frequencyMap, self.apix/float(self.resolution), self.fullMap.shape);
+		self.filteredMap[self.filteredMap<0.0] = 0.0;
+                
+
+
+	
 
 
